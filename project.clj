f4a5983b28813b1b716d6ec31b31d782f62f4cb4
deltam <deltam@gmail.com>
2010-10-19 13:40:26 +0900

    modified lib path

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject trojure "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :main trojure.core
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [compojure "0.4.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.4.1"]
                  [hiccup "0.2.6"]
-                 [ring/ring "0.2.0"]])
+                 [ring/ring "0.2.1"]]
+  )
