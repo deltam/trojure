64805b76da4f87210ad99d753ab9e780800e3f9b
deltam <deltam@gmail.com>
2010-10-19 13:28:25 +0900

    fixed

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject trojure "1.0.0-SNAPSHOT"
   :description "FIXME: write"
+  :main trojure.core
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [compojure "0.4.0-RC3"]
--- src/trojure/core.clj
+++ src/trojure/core.clj
@@ -1,9 +1,11 @@
 (ns trojure.core
+  (:gen-class)
     (:use [compojure.core]
           [hiccup.core]
           [hiccup.form-helpers]
           [ring.util.response :only (redirect)]
           [ring.adapter.jetty :only (run-jetty)]
+          [clojure.contrib.duck-streams :only (reader writer)]
           [clojure.contrib.seq-utils :only (rand-elt)]))
 
 (defn rand-id []
@@ -17,10 +19,22 @@
     (dosync (alter entries conj (struct entry id text)))))
 
 ;; test data
+(def log_file "log.txt")
 (add-entry "hello world")
 (add-entry "clojure さいこうじゃー")
 (add-entry "Perlはぺるるとよむ")
 (add-entry "trojure = tropy + clojure")
+
+(defn file->sexp-list []
+  (let [r (java.io.PushbackReader. (reader log_file))]
+    (take-while identity
+      (repeatedly #(or (read r false false) (.close r))))))
+
+(defn out-log []
+  (with-open [w (writer log_file)]
+      (doseq [et @entries]
+        (binding [*out* w] (prn et)))))
+
 
 (defn get-entry [id]
   (some #(if (= id (% :id)) %) @entries))
@@ -48,10 +62,20 @@
                      (text-area {:rows 10 :cols 50} :text)
                      (submit-button "Post"))))
 
+(defn update-entry [id text]
+  (html-doc ""
+            (form-to [:put "/update"]
+                     (text-area {:rows 10 :cols 50} :text text)
+                     (hidden-field :id id)
+                     (submit-button "Update"))))
+
 (defn create-action [id text]
   (let [new-entry (struct entry id text)]
     (dosync
      (alter entries conj new-entry))))
+
+(defn update-action [id text]
+  (println id text))
 
 (defroutes main-routes
   (GET "/" []
@@ -60,12 +84,16 @@
        (main-body (get-entry id)))
   (GET "/new" []
        (create-entry))
-  (GET "/edit/:id" [id]
-        (println "edit id " id))
   (POST "/save" [text]
         (let [id (rand-id)]
           (create-action id text)
+          (out-log)
           (redirect (str "/entry/" id))))
+  (GET "/edit/:id" [id]
+       (update-entry id ((get-entry id) :text)))
+  (PUT "/update" [id text]
+       (update-action id text)
+       (redirect (str "/entry/" id)))
   (ANY "*" []
        {:status 404, :body "<h1>Page not found</h1>"}))
 
